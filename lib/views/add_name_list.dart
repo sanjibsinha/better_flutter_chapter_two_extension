b28f503d3b7bb9ca3bae7c16baf47ec775e72fb6
sanjibsinha <sanjib12sinha@gmail.com>
2021-06-04 06:47:34 +0530

    styling container more

--- lib/views/add_name_list.dart
+++ lib/views/add_name_list.dart
@@ -10,34 +10,55 @@ class _AddNameListState extends State<AddNameList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Add Expenditures'),
+      appBar: AppBar(
+        title: Text('Add Expenditures'),
+      ),
+      body: Center(
+        child: ListView(
+          children: [
+            Container(
+              margin: EdgeInsets.all(8),
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.blue[50],
+                border: Border.all(
+                  color: Colors.redAccent,
+                ),
+              ),
+              alignment: Alignment.topCenter,
+              child: const Text(
+                'I am at the top and positioned center.',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(8),
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.blue[100],
+                border: Border.all(
+                  color: Colors.black45,
+                  width: 8,
+                ),
+              ),
+              alignment: Alignment.bottomLeft,
+              child: const Text(
+                'I am at the bottom and positioned left.',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+          ],
         ),
-        body: Center(
-          child: ListView(
-            children: [
-              Container(
-                margin: EdgeInsets.all(8),
-                padding: EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  color: Colors.blue[50],
-                  border: Border.all(
-                    color: Colors.redAccent,
-                  ),
-                ),
-                child: const Text(
-                  'Keep Adding Expenditures.',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
